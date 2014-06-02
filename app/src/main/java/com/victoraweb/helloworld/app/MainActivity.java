6b69b66a8bc880d4fd59a4e4fcc7258f9bca72e2
Victor <victoraweb@gmail.com>
2014-06-01 22:02:21 -0300

    Enter and display text

--- app/src/main/java/com/victoraweb/helloworld/app/MainActivity.java
+++ app/src/main/java/com/victoraweb/helloworld/app/MainActivity.java
@@ -1,12 +1,17 @@
 package com.victoraweb.helloworld.app;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public final static String EXTRA_MESSAGE = "com.victoraweb.helloworld.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +37,17 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+
+        String message = editText.getText().toString();
+
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+        startActivity(intent);
     }
 }
